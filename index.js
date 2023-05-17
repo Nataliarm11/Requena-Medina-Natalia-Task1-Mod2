7e5a556f637d69c3386752304203a390876e4f2a
Nataliarm11 <nrequenam1@gmail.com>
2023-05-16 19:10:51 -0500

    version4-task4

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-const checkbox = document.getElementById("checkbox")
-const cartas = document.getElementById ( "cards")
-const inputSearch = document.getElementById("search")
+ const checkbox = document.getElementById("checkbox")
+ const cartas = document.getElementById ( "cards")
+ const inputSearch = document.getElementById("search")
+ 
+ let dataE;
+ let todosEvents;
+ fetch("https://mindhub-xj03.onrender.com/api/amazing")
+ .then ( response => response.json ())
+ .then ( data => {
+  dataE = data
+  todosEvents = data.events
+  console.log (dataE)
+  console.log (todosEvents)
 
- //CHECBOX PARA NO REPETIR
- const category = data.events.map (events => events.category)
- const setCategory = new Set ( category ) 
- const arrayCategory = Array.from ( setCategory)
+  //  //CHECBOX PARA NO REPETIR
+  const category = todosEvents.map (events => events.category)
+  console.log (category)
+  const setCategory = new Set ( category ) 
+  console.log (setCategory)
+  const arrayCategory = Array.from ( setCategory)
+  console.log(arrayCategory)
 
- //CHECBOX CATEGORY 
+//  //CHECBOX CATEGORY 
 
- const funcionReduce = ( acumulador, elementoActual, indice, array ) => {
-    return acumulador += `<div class="form-check form-check-inline">
-                            <input class="form-check-input" type="checkbox" id="${elementoActual}-${indice}" value="${elementoActual}">
-                            <label class="form-check-label" for="${elementoActual}-${indice}">${elementoActual}</label>
-                            </div>`
-  }
- const templateCheckbox = arrayCategory.reduce ( funcionReduce , '' )
+  const funcionReduce = ( acumulador, elementoActual, indice, array ) => {
+     return acumulador += `<div class="form-check form-check-inline">
+                             <input class="form-check-input" type="checkbox" id="${elementoActual}-${indice}" value="${elementoActual}">
+                             <label class="form-check-label" for="${elementoActual}-${indice}">${elementoActual}</label>
+                             </div>`
+   }
+  const templateCheckbox = arrayCategory.reduce ( funcionReduce , '' )
 
- //imprimir
- checkbox.innerHTML = ( templateCheckbox )
+//  //imprimir
+  checkbox.innerHTML = ( templateCheckbox )
 
- //para las cartas  
- function template ( lista ) {
+
+  //  //para las cartas  
+  function template ( lista ) {
     return lista.reduce( ( acc, act ) => {
         return acc +=  `<article class="card col-6 col-md-5 col-lg-4 col-xl-2">
-        <img src="${act.image}" class="card-img-top" alt="books">
+        <img src="${act.image}" class="card-img-top object-fit-fill p-2" alt="books">
         <div class="card-body">
           <h5 class="card-title text-dark text-body-emphasis">${act.name}</h5>
           <p class="card-text">date: ${act.date}</p>
@@ -38,72 +52,60 @@ const inputSearch = document.getElementById("search")
         </div>
       </article>`
     }, '' ) //empiza string vacio
-}
 
-cartas.innerHTML = template (data.events)
+  }
+  cartas.innerHTML = template (todosEvents)
 
 
-// PONER EN FUNCIONAMIENTO LOS CHECKBOX Y EL SEARCH
-//CUANDO EL USUARIO REALIZA UN EVENTO
-
-//search
-
- inputSearch.addEventListener ( 'input', () => {
-   const busqueda = filterTittle ( data.events, inputSearch.value)
-   const inputs = document.querySelectorAll("input[type='checkbox']");
-   const anyChecked = false
-   for (let i = 0; i < inputs.length; i++) {   
-    if(inputs[i].checked) {
-      const porCategory = filtrarCategory (busqueda, inputs[i].value)
-      cartas.innerHTML= template (porCategory , cartas)
-      anyChecked = true
-    }
-  }   
-  if(anyChecked == false) {
-    cartas.innerHTML= template (busqueda , cartas)
-  }   
- })
+  inputSearch.addEventListener ( 'input', () => {
+    const busqueda = filterTittle ( todosEvents, inputSearch.value)
+    const inputs = document.querySelectorAll("input[type='checkbox']");
+    const anyChecked = false
+    for (let i = 0; i < inputs.length; i++) {   
+     if(inputs[i].checked) {
+       const porCategory = filtrarCategory (busqueda, inputs[i].value)
+       cartas.innerHTML= template (porCategory , cartas)
+       anyChecked = true
+     }
+   }   
+   if(anyChecked == false) {
+     cartas.innerHTML= template (busqueda , cartas)
+   }   
+  })
 
  
 
-let checkboxElems = document.querySelectorAll("input[type='checkbox']");
+ let checkboxElems = document.querySelectorAll("input[type='checkbox']");
 
-for (let i = 0; i < checkboxElems.length; i++) {
-  checkboxElems[i].addEventListener("click", (e) => {
-    const busqueda = filterTittle ( data.events, inputSearch.value)
-    if (e.target.checked) {
-      const porCategory = filtrarCategory (busqueda, e.target.value)
+ for (let i = 0; i < checkboxElems.length; i++) {
+   checkboxElems[i].addEventListener("click", (e) => {
+     const busqueda = filterTittle ( todosEvents, inputSearch.value)
+     if (e.target.checked) {
+       const porCategory = filtrarCategory (busqueda, e.target.value)
       
-      cartas.innerHTML= template (porCategory , cartas)
-    } else {
-      cartas.innerHTML= template (busqueda , cartas)
-    }
-  });
-}
-
- function filtrarCategory ( filterData, search ) {
-      if (filterData.length == 0 ) {
-          return data.events
-      }
-      const filtered = filterData.filter((el) => el.category == search);
-      return filtered
+       cartas.innerHTML= template (porCategory , cartas)
+     } else {
+       cartas.innerHTML= template (busqueda , cartas)
+     }
+   });
  }
 
-  function filterTittle(array, search) {
-   return array.filter(palabra => palabra.name.toLowerCase().includes(search.toLowerCase()));  
+  function filtrarCategory ( filterData, search ) {
+       if (filterData.length == 0 ) {
+           return todosEvents.events
+       } 
+       const filtered = filterData.filter((el) => el.category == search);
+       return filtered 
+       
   }
 
+   function filterTittle(array, search) {
+    return array.filter(palabra => palabra.name.toLowerCase().includes(search.toLowerCase()));  
+   }
 
 
-
-
-
-
-
-
-
-
-
+  })
+  .catch( error => console.log (error))
 
 
 
--- upcoming.js
+++ upcoming.js
@@ -1,26 +1,29 @@
-
-// DE NUEVO
 const checkbox = document.getElementById("checkbox")
-console.log (document.getElementById("checkbox"))
-
-
 const cartas = document.getElementById ( "cardsUpcoming")
-const todosEvents = data.events
-const eventosFuturos = todosEvents.filter ( events => events.date > data.currentDate)
-
-
 const inputSearch = document.getElementById("search")
 
+let dataE;
+let todosEvents;
+let eventosFuturos;
+fetch("https://mindhub-xj03.onrender.com/api/amazing")
+.then ( response => response.json ())
+.then ( data => {
+ dataE = data
+ todosEvents = data.events
+ eventosFuturos= todosEvents.filter ( events => events.date > data.currentDate)
+ console.log (eventosFuturos)
+ console.log (dataE)
+ console.log (todosEvents)
 
- //CHECBOX PARA NO REPETIR
-
+ //  //CHECBOX PARA NO REPETIR
  const category = eventosFuturos.map (events => events.category)
+ console.log (category)
  const setCategory = new Set ( category ) 
+ console.log (setCategory)
  const arrayCategory = Array.from ( setCategory)
+ console.log(arrayCategory)
 
-
-
- //CHECBOX CATEGORY 
+//  //CHECBOX CATEGORY 
 
  const funcionReduce = ( acumulador, elementoActual, indice, array ) => {
     return acumulador += `<div class="form-check form-check-inline">
@@ -28,48 +31,34 @@ const inputSearch = document.getElementById("search")
                             <label class="form-check-label" for="${elementoActual}-${indice}">${elementoActual}</label>
                             </div>`
   }
-
-
  const templateCheckbox = arrayCategory.reduce ( funcionReduce , '' )
 
- 
-
- //imprimir
+//  //imprimir
  checkbox.innerHTML = ( templateCheckbox )
 
 
-
- //para las cartas 
-
- 
+ //  //para las cartas  
  function template ( lista ) {
-    return lista.reduce( ( acc, act ) => {
-        return acc +=  `<article class="card col-6 col-md-5 col-lg-4 col-xl-2">
-        <img src="${act.image}" class="card-img-top" alt="books">
-        <div class="card-body">
-          <h5 class="card-title text-dark text-body-emphasis">${act.name}</h5>
-          <p class="card-text">date: ${act.date}</p>
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">price: ${act.price}</li>
-          <li class="list-group-item">place: ${act.place}</li>
-        </ul>
-        <div class="card-body">
-          <a href="./details.html?id=${act._id}" class="card-link">See More</a>
-        </div>
-      </article>`
-    }, '' ) //empiza string vacio
-}
+   return lista.reduce( ( acc, act ) => {
+       return acc +=  `<article class="card col-6 col-md-5 col-lg-4 col-xl-2">
+       <img src="${act.image}" class="card-img-top object-fit-fill p-2">
+       <div class="card-body">
+         <h5 class="card-title text-dark text-body-emphasis">${act.name}</h5>
+         <p class="card-text">date: ${act.date}</p>
+       </div>
+       <ul class="list-group list-group-flush">
+         <li class="list-group-item">price: ${act.price}</li>
+         <li class="list-group-item">place: ${act.place}</li>
+       </ul>
+       <div class="card-body">
+         <a href="./details.html?id=${act._id}" class="card-link">See More</a>
+       </div>
+     </article>`
+   }, '' ) //empiza string vacio
 
-cartas.innerHTML = template (eventosFuturos)
+ }
+ cartas.innerHTML = template (eventosFuturos)
 
-
-// PONER EN FUNCIONAMIENTO LOS CHECKBOX
-
-
-//Checkbox cuando el usuario realiza un check
-
-//search
 
  inputSearch.addEventListener ( 'input', () => {
    const busqueda = filterTittle ( eventosFuturos, inputSearch.value)
@@ -87,6 +76,8 @@ cartas.innerHTML = template (eventosFuturos)
   }   
  })
 
+
+
 let checkboxElems = document.querySelectorAll("input[type='checkbox']");
 
 for (let i = 0; i < checkboxElems.length; i++) {
@@ -94,7 +85,7 @@ for (let i = 0; i < checkboxElems.length; i++) {
     const busqueda = filterTittle ( eventosFuturos, inputSearch.value)
     if (e.target.checked) {
       const porCategory = filtrarCategory (busqueda, e.target.value)
-      
+     
       cartas.innerHTML= template (porCategory , cartas)
     } else {
       cartas.innerHTML= template (busqueda , cartas)
@@ -104,7 +95,7 @@ for (let i = 0; i < checkboxElems.length; i++) {
 
  function filtrarCategory ( filterData, search ) {
       if (filterData.length == 0 ) {
-          return eventosFuturos
+          return eventosFuturos.events
       }
       const filtered = filterData.filter((el) => el.category == search);
       return filtered
@@ -113,6 +104,13 @@ for (let i = 0; i < checkboxElems.length; i++) {
   function filterTittle(array, search) {
    return array.filter(palabra => palabra.name.toLowerCase().includes(search.toLowerCase()));  
   }
+
+
+ })
+ .catch( error => console.log (error))
+
+
+
 
 
 
